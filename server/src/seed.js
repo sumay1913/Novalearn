ecbe167730fbabb2c2c454af592ff05ac3f9f5fe
Mayank Rajput <mayankrajput3440@gmail.com>
2026-06-13 01:02:30 +0530

    Update seed.js

--- server/src/seed.js
+++ server/src/seed.js
@@ -1,9 +1,3 @@
-console.log(
-  courses[1].lessons.map(l => ({
-    title: l.title,
-    videoUrl: l.videoUrl
-  }))
-);
 import "dotenv/config";
 import mongoose from "mongoose";
 import User from "./models/User.js";
